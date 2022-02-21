c8e0d9b90d8f52ecce97eb1d0e1072760cff6b57
Israel  Molestina <israel@loxo-solutions.com>
2022-02-21 17:30:00 -0500

    pushing models for users and current trips

--- shred/shred/settings.py
+++ shred/shred/settings.py
@@ -74,11 +74,29 @@ WSGI_APPLICATION = 'shred.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'shredb',
+
+        'USER': 'shredb_user',
+
+        'PASSWORD': '12345!@#$%',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
+# superuser
+#username: israelmolestina
+# email molestinaisrael@gmail.com
+# password 12345!@#$%
 
 
 # Password validation
--- shred/shredApp/models.py
+++ shred/shredApp/models.py
@@ -1,3 +1,20 @@
+import email
+from statistics import mode
+from weakref import ReferenceType
 from django.db import models
 
 # Create your models here.
+
+
+class users(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    riding = models.CharField(max_length=50)
+    stance = models.CharField(max_length=50)
+
+
+class current_trip(models.Model):
+    location = models.CharField(max_length=50)
+    airbnb = models.CharField(max_length=50)
+    airbnb = models.CharField(max_length=50)
