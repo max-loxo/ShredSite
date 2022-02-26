e44c9ea7d2f9f92551396a21030758de6568307e
Israel  Molestina <israel@loxo-solutions.com>
2022-02-25 23:42:23 -0500

    adding resort table

--- shred/shredApp/models.py
+++ shred/shredApp/models.py
@@ -1,4 +1,4 @@
-import email
+from random import choices
 from statistics import mode
 from weakref import ReferenceType
 from django.db import models
@@ -7,14 +7,53 @@ from django.db import models
 
 
 class User(models.Model):
+    REGULAR = 'RG'
+    GOOFY = 'GF'
+    STANCE_CHOICES = [
+        (REGULAR, 'REGULAR'),
+        (GOOFY, 'GOOFY'),
+    ]
+    BOARD = 'BRD'
+    SKI = 'SKI'
+    RIDING_CHOICES = [
+        (BOARD, 'BOARD'),
+        (SKI, 'SKI'),
+    ]
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    riding = models.CharField(max_length=50)
-    stance = models.CharField(max_length=50)
+    riding = models.CharField(
+        max_length=3,
+        choices=RIDING_CHOICES,
+        default=BOARD,
+        blank=False
+    ),
+    stance = models.CharField(
+        max_length=3,
+        choices=STANCE_CHOICES,
+        default=REGULAR,
+        blank=False
+    ),
 
 
-class CurrentTrip(models.Model):
-    location = models.CharField(max_length=50)
-    airbnb = models.CharField(max_length=50)
-    airbnb = models.CharField(max_length=50)
+class Trips(models.Model):
+    location = models.CharField(max_length=100)
+    airbnb = models.CharField(max_length=5000)
+    date = models.DateField()
+    user = models.ManyToManyField(User)
+
+class Resort(models.Model):
+    EPIC = 'EPC'
+    ICON = 'IKN'
+    PASS_CHOICES = [
+        (EPIC, 'EPIC'),
+        (ICON, 'ICON'),
+    ],
+    name = models.CharField(max_length=100)
+    base_elevation = models.IntegerField()
+    peak_elevation = models.IntegerField()
+    price = models.IntegerField()
+    trails = models.IntegerField()
+    url = models.i
+    
